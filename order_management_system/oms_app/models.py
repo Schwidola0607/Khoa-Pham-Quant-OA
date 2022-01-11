86931cd6aa0bf2e2d8c995c27092f4f7afe31cda
Schwidola0607 <khoadangpham82944@gmail.com>
2022-01-10 23:13:03 -0500

    order processor done not fully tested added price field for models added helper for order and execution confirmation

--- order_management_system/oms_app/models.py
+++ order_management_system/oms_app/models.py
@@ -1,10 +1,15 @@
 import uuid
+from django.core.checks import messages
+import pytz
+
 from django.db import models
 from django.db.models.enums import Choices
 from django.utils.translation import gettext as _
 from datetime import datetime
-# Create your models here.
+# Price dictionary
+Price = {}
 class Order(models.Model):
+  #TODO shorten the UUID
   uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   ticker = models.CharField(max_length=200)
   quantity = models.PositiveIntegerField()
@@ -31,29 +36,49 @@ class Order(models.Model):
     choices=Action.choices,
     default=Action.PENDING,
   )
-  
+  price = models.PositiveIntegerField(default=0)
   placed_time = models.DateTimeField(auto_now_add=True)
-  expiry = models.DateTimeField(default=datetime.max)
+  expiry = models.DateTimeField(default=pytz.utc.localize(datetime.max))
   
   def get_order_type(self):
     """
       helper function to get order type from the enum class OrderType
     """
     return self.OrderType(self.order_type).name
+
   def get_action(self):
     """
       helper fucntion to get action type from the enum class Action
     """
     return self.Action(self.action).name
-  def __str__(self):
+
+  def placed_confirm(self):
     """
-      Order to string function
+      Order placement confirmation
     """
     return (
-      f'Order {self.uuid} executed\n' 
-      f'{self.get_order_type()} {self.get_action()} {self.quantity} {self.ticker}\n' 
+      f'Trade order placed!:\n'
+      f'{self.get_order_type()} {self.uuid} is through\n' 
+      f'{self.get_action()} {self.quantity}'
+      f'{self.ticker} shares at {self.price}\n'
       f'At {self.placed_time}, expired at {self.expiry}'
     )
+  def executed_confirm(self, num_shares, price_per_shares):
+    """
+      Order execution confirmation
+    """  
+    return (
+      f'Successfully matched!\n'
+      f'{self.get_order_type()} {self.uuid} executed\n'
+      f'{self.get_action()} {num_shares}'
+      f'@ {price_per_shares}'
+      f'at {datetime.utcnow().replace(tzinfo=pytz.utc)}\n'
+    )
+  def __str__(self):
+    """
+      Order class to_string()
+    """
+    return self.placed_confirm()
   def save(self, *args, **kwargs): 
     """
       Overriding save() method for model subclass order
@@ -61,12 +86,83 @@ class Order(models.Model):
     if (self.order_type != 'mar' and
           self.order_type != 'lim'):
       raise Exception(
-        "invalid order type, try 'mar for market' or 'lim' for limit order argument"
+        "Invalid order type, try 'mar for market' or 'lim' for limit order argument"
       )
 
     if (self.action != 'buy' and
           self.action != 'sell'):
       raise Exception(
-        "invalid action, try 'buy' and 'sell' as argument"
+        "Invalid action, try 'buy' and 'sell' as argument"
       )
     super().save(*args, **kwargs)  
+  
+  #TODO finish
+  def compat(self, ord):
+    """
+      Return a tuple of matching compatibility and agreement price between two orders 
+    """
+
+    if self.action == ord.action:
+      return False
+
+    #if both orders are market order 
+    if self.order_type == 'mar' and ord.order_type == 'mar':
+      # there is a previous price in the stock market
+      if self.ticker in Price:
+        return True, Price[self.ticker]
+      # there is not
+      return False
+    
+    # istant agreement if only one of the order is market order
+    if self.order_type == 'mar':
+      return True, ord.price
+    if ord.order_type == 'mar':
+      return True, self.price
+    
+    # if both are limit orders, compare buy-max / sell-min price
+    if self.action == 'buy':
+      return self.price > ord.price
+    if self.action == 'sell':
+      return self.price < ord.price
+
+  def exchange(self, ord):
+    """
+      Exchange between two orders
+    """
+    if self.compat(ord) is False:
+      raise Exception(
+        f'Wrong argument Order {self.uuid} and {ord.uuid}'
+      )
+    compatability, price_per_shares = self.compat(ord)
+    num_shares = min(self.quantity, ord.quantity)
+    self.quantity -= num_shares
+    ord.quantity -= num_shares
+
+    return self.executed_confirm(num_shares, price_per_shares)
+
+  def process(self):
+    """
+      Order's processer function, the oms's automatic broker
+    """
+
+    orders = Order.objects.filter(ticker=self.ticker)
+    # print(orders)
+    message=""
+    for order in orders:
+      # print(order)
+      if self.compat(order) is not False:
+        # print("Debugger 1:")
+        compatitbility, Price[self.ticker] = self.compat(order)
+        message+=self.exchange(order) + '\n'
+        if order.quantity == 0:
+          order.delete()
+        if self.quantity == 0:
+          self.delete()
+          break
+    
+    return message  
+
+          
+    
+
+        
